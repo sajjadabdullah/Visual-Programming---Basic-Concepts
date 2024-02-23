742d29b3459561941bf6cca1a135ca49d9ab2256
Sajjad Abdullah <sajjad.abdullah@gmail.com>
2024-02-23 09:47:06 +0500

    aa

--- 007 - Functions Practice/Program.cs
+++ 007 - Functions Practice/Program.cs
@@ -10,6 +10,32 @@ namespace _007___Functions_Practice
     {
         static void Main(string[] args)
         {
+            //Car.Greet();
+            Car c1=new Car(); //object is declared & initialized in memory
+            c1.Greet(20, '%');
+            int z = c1.Sum(10, 20);
+            Console.WriteLine($"Sum is {z} inside Main function.");
+
+            Console.ReadLine();
         }
+    }
+
+    class Car
+    {
+        public void Greet(int n, char ch)
+        {
+            for (int i = 1; i <= n; i++)
+                Console.Write($"{ch}");
+            Console.WriteLine("\nWelcome BS 5th");
+        }
+
+        public int Sum(int a, int b)
+        {
+            int c = a + b;
+            Console.WriteLine($"Sum={c}");
+            return c;
+        }
+
+
     }
 }

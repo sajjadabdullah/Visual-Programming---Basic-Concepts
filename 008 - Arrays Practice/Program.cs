28c393d6397c863b0f89b6519de266ace3e3fa8a
Sajjad Abdullah <sajjad.abdullah@gmail.com>
2024-03-08 09:45:19 +0500

    aa

---
+++ 008 - Arrays Practice/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _008___Arrays_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] x = { 10, 15, 20, 30, 35};
+            //int[] x = new int[5];
+            //for (int i = 0; i <= 4; i++)
+            //{
+            //    Console.WriteLine(x[i]);
+            //}
+
+
+
+            //int[] x = { 10, 15, 20, 30, 35 };
+            //Console.WriteLine(x[2]);
+            //x[2] = 50;
+            //Console.WriteLine(x[2]);
+            //for (int i = 0; i <= 4; i++)
+            //{
+            //    Console.WriteLine(x[i]);
+            //}
+
+
+
+            //int[] x = new int[10];
+            //Random r = new Random();
+            //for (int i = 0; i <= 9; i++)
+            //{
+            //    x[i] = r.Next(1, 101);
+            //    Console.Write(x[i] + " ");
+            //}
+            //int sum = 0;
+            //int max = x[0];
+            //int min = x[0];
+            //for (int i = 0; i < x.Length; i++)
+            //{
+            //    sum += x[i]; // sum= sum + x[i];
+            //    //find maximum
+            //    if (x[i] > max)
+            //        max = x[i];
+            //    //find minimum
+            //    if (x[i] < min)
+            //        min = x[i];
+            //}
+            //double avg = (double)sum / x.Length;
+            //Console.WriteLine($"\nSum of array is {sum}");
+            //Console.WriteLine($"Average of array is {avg}");
+            //Console.WriteLine($"Maximum of array is {max}");
+            //Console.WriteLine($"Minimum of array is {min}");
+
+
+            //// 2-D Arrays
+            //int[,] x = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 10, 11, 12 } };
+            ////int[,] x = new int[4,3];
+            ////Console.WriteLine(x[1, 0]);
+            //for (int r = 0; r <= 3; r++)
+            //{
+            //    for (int c = 0; c <= 2; c++)
+            //    {
+            //        Console.Write(x[r, c] + "\t");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+
+            //// 3-D Arrays
+            //int[,,] x = new int[3, 4, 3];
+
+
+
+            ////Understanding Lists
+            //List<int> x = new List<int>();
+            ////List<int> x = new List<int>() { 10, 20, 30, 40, 50 };
+            //x.Add(10);
+            //x.Add(20);
+            //x.Add(30);
+            //x.Add(40);
+            //x.Remove(20);
+            //Console.WriteLine(x.Count);
+            //for (int i = 0; i <= 3; i++)
+            //    Console.WriteLine(x[i]);
+            //foreach (int it in x)
+            //{
+            //    Console.WriteLine(it);
+            //}
+            //bool a = x.Contains(35);
+            //if (a == true)
+            //    Console.WriteLine("Found");
+            //else
+            //    Console.WriteLine("NOT Found");
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 009 - Structures Practice/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _009___Structures_Practice
+{
+    class Program
+    {
+        struct Book
+        {
+            public string Title;
+            public int Pages;
+            public double Price;
+
+            public Book(string title, int pages, double price)
+            {
+                Title = title;
+                Pages = pages;
+                Price = price;
+            }
+
+            public void print()
+            {
+                Console.WriteLine($"Title={Title}");
+                Console.WriteLine($"Pages={Pages}");
+                Console.WriteLine($"Price={Price}");
+            }
+        }
+
+        static Book GetExpensiveBook(Book b1, Book b2)
+        {
+            Book x;
+            if (b1.Price > b2.Price)
+                x = b1;
+            else
+                x = b2;
+
+            return x;
+        }
+
+        static void Main(string[] args)
+        {
+            Book a = new Book("Data Structures",1000,30.5);
+            Book b = new Book("Intro to C#", 500, 35.5);
+            Console.WriteLine("Book 1 Details");
+            a.print();
+            Console.WriteLine("Book 2 Details");
+            b.print();
+
+            Book exBook = GetExpensiveBook(a, b);
+            Console.WriteLine("Expense Book Details");
+            exBook.print();
+
+            Console.ReadLine();
+        }
+    }
+}
